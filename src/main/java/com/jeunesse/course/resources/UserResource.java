48979b5821bd0aba671d36a6f8f1b65c2209dd68
fanecovsf <gustavo_h07@hotmail.com>
2023-07-28 06:18:22 -0300

    🆕 Feature:
    User and UserResource implemented

---
+++ src/main/java/com/jeunesse/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.jeunesse.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeunesse.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Gustavo", "gustavo@gmail.com", "9999999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
