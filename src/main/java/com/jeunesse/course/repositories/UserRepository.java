7bd67e0c6f3b22a77ebb266db0eddda02f1b471c
fanecovsf <gustavo_h07@hotmail.com>
2023-07-28 06:47:38 -0300

    🆕 Feature:
    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/jeunesse/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jeunesse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeunesse.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
